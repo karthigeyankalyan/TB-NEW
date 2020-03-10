e4a5cb5036940799f608831c2435c51fedc9f1e7
karthigeyankalyan <karthigeyankalyan@gmail.com>
2020-03-11 00:47:58 +0530

    Checking

--- src/app.py
+++ src/app.py
@@ -498,10 +498,12 @@ def multi_receipt_form(user_id):
                                     new_credit_balance = int(clearing_balance_credit) + int(cl_credit_old)
                                     new_debit_balance = int(cl_debit_old)
                     elif int(clearing_balance_credit) >= 0:
+                        print(int(cl_debit_old) >= 0)
                         if int(cl_debit_old) >= 0:
+                            print(int(clearing_balance_debit) == 0)
                             if int(clearing_balance_debit) == 0:
+                                print(int(cl_credit_old) == 0)
                                 if int(cl_credit_old) == 0:
-                                    print(int(cl_debit_old) >= int(clearing_balance_credit))
                                     if int(cl_debit_old) >= int(clearing_balance_credit):
                                         print(3)
                                         new_debit_balance = int(cl_debit_old) - int(clearing_balance_credit)
